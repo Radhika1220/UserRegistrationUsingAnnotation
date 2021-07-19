621f54883548c3c455570e5d17d6e11574c3cc57
Radhika <radhika.shankar1220@gmail.com>
2021-07-19 13:28:09 +0530

    [Radhika] using annotation

---
+++ UserRegistrationUsingAnnotation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UserRegistrationAnnotations;
+
+namespace UserRegistrationUsingAnnotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Validation validator = new Validation();
+            validator.GetInput();
+        }
+    }
+}
---
+++ UserRegistrationUsingAnnotation/Validator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserRegistrationUsingAnnotation;
+
+namespace UserRegistrationAnnotations
+{
+    public class Validation
+    {
+        /// <summary>
+        /// Annoations
+        /// </summary>
+        Annotation annotation = new Annotation();
+        public void GetInput()
+        {
+            Console.WriteLine("Enter first name :");
+            annotation.firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last name : ");
+            annotation.lastName = Console.ReadLine();
+            Console.WriteLine("Enter Email Id : ");
+            annotation.emailId = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number :");
+            annotation.phoneNum = Console.ReadLine();
+            Console.WriteLine("Enter Password : ");
+            annotation.password = Console.ReadLine();
+            TestAnnotationFields(annotation);
+
+        }
+        public void TestAnnotationFields(Annotation annotation)
+        {
+            ValidationContext validationContext = new ValidationContext(annotation, null, null);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(annotation, validationContext, validationResults, true);
+            if (!valid)
+            {
+                foreach (ValidationResult validationResult in validationResults)
+                {
+                    Console.WriteLine("{0}", validationResult.ErrorMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Satisfied all validations");
+            }
+
+        }
+
+    }
+}
